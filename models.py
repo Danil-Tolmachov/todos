4b744e99e463799498ebb9a62cd38691d375e942
Danil Tolmachov <danil.tolmachov45@gmail.com>
2023-04-11 13:53:53 +0300

    create models

--- models.py
+++ models.py
@@ -1,19 +1,28 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from database import Base
 
 
+
 class User(Base):
-    
     __tablename__ = 'users'
-    id = Column('id', Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     
-    ###
+    username = Column(String)
+    email = Column(String)
+    hashed_password = Column(String)
+    
+    todos = relationship('Todo', back_populates='user')
 
 
 class Todo(Base):
-
     __tablename__ = 'todos'
-    id = Column('id', Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
 
-    ###
+    title = Column(String)
+    description = Column(String)
+    importance = Column(Integer, default=1)
+
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='todos')
